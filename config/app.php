5bde685727fe9df43cea795ce0ab93942e4d063c
NenengAprianti <aprhyc8@gmail.com>
2018-05-21 05:01:54 +0700

    .

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     */
 
     // 'name' => env('APP_NAME', 'Laravel'),
-    'name' => 'KRS ONLINE UMM',
+    'name' => 'KRS ONLINE',
 
     /*
     |--------------------------------------------------------------------------
