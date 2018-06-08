c4217acf72a5746e2d91f6520c847a077a67d9d0
NenengAprianti <aprhyc8@gmail.com>
2018-06-08 21:09:02 +0700

    .

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,5 +1,23 @@
-@extends('layouts.admin')
+@extends('layouts.app')
 
 @section('content')
-INI HALAMAN ADMIN
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">ADMIN Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in as <strong>USER</strong>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
