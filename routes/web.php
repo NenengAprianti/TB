2915c22db8174b591eae6b0f6fd1502ed52a15bb
NenengAprianti <aprhyc8@gmail.com>
2018-06-12 15:17:09 +0700

    ubah

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,13 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Auth::routes();
 
-Route::get('/', 'LoginController@index');
+// Route::get('/', 'LoginController@index');
 
 Route::get('/home', 'HomeController@index');
 Route::get('/user/logut', 'Auth\LoginController@userLogout')->name('user.logout');
@@ -29,6 +29,12 @@ Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.log
 Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
 Route::get('/', 'AdminController@index')->name('admin.dashboard');
 Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+
+// Password reset routes
+  Route::post('/password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+  Route::get('/password/reset', 'Auth\AdminForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+  Route::post('/password/reset', 'Auth\AdminResetPasswordController@reset');
+  Route::get('/password/reset/{token}', 'Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');
 });
 
 Route::get('/admin/dosen', 'dosenController@index');
